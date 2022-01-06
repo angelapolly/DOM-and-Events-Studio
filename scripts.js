8256aaf1fb79a2333586a9d52f554155e810054f
Angela Polly <agpolly@gmail.com>
2022-01-06 14:52:51 -0500

    initial changes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+    console.log("window loaded");
+
+
+const takeOff = this.document.getElementById("takeOff");
+
+takeOff.addEventListener("click", function(){
+    // console.log("The shuttle is ready for take off");
+    results = window.confirm("Is the shuttle is ready for take off?");
+    // if (results){
+    //     status.innerHTML = "Shuttle in flight";
+    // }
+});
+
+});
